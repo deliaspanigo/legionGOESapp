a59c467e17204f643ebb4d8fd0d0a29624b769a1
unknown <david.elias949@gmail.com>
2026-05-16 22:58:35 +0200

    Release: Version 0.0.8

--- _script004_github_version.R
+++ _script004_github_version.R
@@ -1,12 +1,12 @@
 # 1. Preparar y Guardar los cambios (incluyendo el DESCRIPTION limpio)
 git add .
-git commit -m "Release: Version 0.0.7"
+git commit -m "Release: Version 0.0.8"
 
 # 2. Etiquetar la versión
 # Borramos el tag local por si acaso ya se creó con error antes
 #git tag -d v0.9.12 2>/dev/null
-git tag -d v0.0.7 | Out-Null
-git tag -a v0.0.7 -m "Version  0.0.7"
+git tag -d v0.0.8 | Out-Null
+git tag -a v0.0.8 -m "Version  0.0.8"
 
 # 3. Subir cambios y etiquetas a GitHub
 git push origin main --follow-tags
